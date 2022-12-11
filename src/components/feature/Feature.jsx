d5c1519b123ef0b18d63088d48c8c72bc1e140ef
niteshkr034 <81917957+niteshkr034@users.noreply.github.com>
2022-12-11 18:37:13 +0530

    possibility media query

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -8,8 +8,8 @@ return (
         <div/>
         <h1>{title}</h1>
     </div>
-<div className="gpt3__feature-container_feature-text">
-    {text}
+<div className="gpt3__features-container_feature-text">
+   <p>{text}</p> 
 </div>
 </div>    
 )
